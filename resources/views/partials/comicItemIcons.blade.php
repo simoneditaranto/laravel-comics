7ecc2a0917ce606d75541ba2f0114d86d9815b15
simoneditaranto <simodita99@gmail.com>
2024-04-09 13:43:43 +0200

    feat: bonus completato

---
+++ resources/views/partials/comicItemIcons.blade.php
@@ -0,0 +1,22 @@
+<div id="item-icons">
+
+    <div class="container">
+        <div class="card-icon">
+            <h3>digital comics</h3>
+            <img src="" alt="">
+        </div>
+        <div class="card-icon">
+            <h3>digital comics</h3>
+            <img src="" alt="">
+        </div>
+        <div class="card-icon">
+            <h3>digital comics</h3>
+            <img src="" alt="">
+        </div>
+        <div class="card-icon">
+            <h3>digital comics</h3>
+            <img src="" alt="">
+        </div>
+    </div>
+
+</div>
